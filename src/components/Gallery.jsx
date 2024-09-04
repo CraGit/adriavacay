5faba32a3bfd986d0c5f26cf9fb1f895803112bf
Kreya <crashaw@gmail.com>
2024-09-04 12:09:33 +0200

    chore: Add PartialDiv component for hiding and showing content

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -14,6 +14,7 @@ import SmallHeading from "./SmallHeading";
 import "yet-another-react-lightbox/plugins/captions.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
 import "yet-another-react-lightbox/styles.css";
+import PartialDiv from "./PartialDiv";
 
 export default function PhotoGallery({ photos, overtitle, heading }) {
   const [index, setIndex] = useState(-1);
@@ -24,25 +25,27 @@ export default function PhotoGallery({ photos, overtitle, heading }) {
   }, []);
 
   return (
-    <div className="">
-      <div className="text-center lg:pb-[60px] pb-[40px]">
-        <SmallHeading heading={heading} />
+    <PartialDiv>
+      <div className="">
+        <div className="text-center lg:pb-[60px] pb-[40px]">
+          <SmallHeading heading={heading} />
+        </div>
+        <PhotoAlbum
+          key={width} // Add key prop to force re-render on width change
+          photos={photos}
+          layout="masonry"
+          columns={3}
+          targetRowHeight={150}
+          onClick={({ index }) => setIndex(index)}
+        />
+        <Lightbox
+          slides={photos}
+          open={index >= 0}
+          index={index}
+          close={() => setIndex(-1)}
+          plugins={[Fullscreen, Slideshow, Thumbnails, Zoom, Captions]}
+        />
       </div>
-      <PhotoAlbum
-        key={width} // Add key prop to force re-render on width change
-        photos={photos}
-        layout="masonry"
-        columns={3}
-        targetRowHeight={150}
-        onClick={({ index }) => setIndex(index)}
-      />
-      <Lightbox
-        slides={photos}
-        open={index >= 0}
-        index={index}
-        close={() => setIndex(-1)}
-        plugins={[Fullscreen, Slideshow, Thumbnails, Zoom, Captions]}
-      />
-    </div>
+    </PartialDiv>
   );
 }
