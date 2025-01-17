27f7a43504d1bd17c35c448c061eb45a720334d0
Kreya <crashaw@gmail.com>
2025-01-17 14:48:58 +0100

    maknut defaultni local iz sitemape

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,13 @@ module.exports = {
   generateRobotsTxt: true, // (optional)
   // ...other options
   exclude: ["/api/*", "/slice-simulator", "/message-sent"],
+  transform: async (config, path) => {
+    return {
+      loc: path.replace("/en-us", ""), // Remove `/en-us` from paths - could get value from i18n config to make it dynamic
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
 };
